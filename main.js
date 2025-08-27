25c1ecb7fc988ea80fc4f8e77f7baebb1200cd5b
mugamaldev <mugamaldev>
2025-08-27 22:57:06 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,168 @@
+
+
+
+//console.log('Hello Khaled your age today is 20');  Montsar => 20 Samaa => 20 Khaled => 20
+// Variables
+/* 
+    var     function scoped
+    let     block scope
+    const   block scoped not modified
+*/
+
+
+// const birthDate = 1994;
+// birthDate = 1995;
+
+
+// console.log(birthDate);
+// #####################################
+// Data types
+//    String | Number | Boolean | Undefined | Null (object)   (( typeof() )) 
+/* var firstName = 'Muhammad';
+var lastName = 'Gamal';
+var anyNumb = 901241284;
+
+console.log(typeof(anyNumb)); 
+// true | false 
+var isAdmin = false; // True == 1 false == 0
+console.log(typeof(isAdmin)); */
+// var myBalance = null; // insta
+// var myAccountBank  ; // instapay
+// console.log(typeof(myAccountBank)); // بكرة المناديل
+
+// + - / * %     
+// var myName = "muhammad";
+// var age = 60;
+// // console.log(`${myName}`);
+// console.log(`My name is ${myName} and my age is ${age}`);
+// console.log("My name is " + myName + "and my age is " + age);
+
+// var myAdmins = "admin";
+// var mySubAdmins = "sub admins";
+// var myLoginData = 'Khaled';
+
+// if(myStatus == "admin") {
+//     console.log("Hello Admin");
+// } else {
+//     console.log("Hello User");
+// }
+
+// if(myLoginData == myAdmins) {
+//     console.log("Hello Admin");
+// } else if(myLoginData == mySubAdmins) {
+//     console.log('Hello sub admin');
+// } else {
+//     console.log('hello users');
+// }
+
+// if(myLoginData == myAdmins) {
+//     console.log("Hello Admin");
+// }else if(myLoginData == mySubAdmins) {
+//     console.log("Hello sub admins");
+// } else {
+//     console.log(`Hello ${myLoginData}`);
+// }
+
+// #### NEVER USE THIS WAY 
+// if(myLoginData != myAdmins) {
+//     console.log("Hello Admin")
+//  } else {
+//  }
+//  if(myLoginData != mySubAdmins) {
+//     console.log("Hello sub admins");
+// }else {
+
+// }
+// number / boolean / == / ===
+// var num1 = 1;
+// var num2 = "1";
+
+// if(num1 == num2) {
+//     console.log("they are equals");
+//     console.log(typeof(num2));
+// }else {
+//     console.log("They are not equals");
+// }
+
+// if(num2) {
+//     console.log("Working");
+// } else {
+//     console.log("This is a true number");
+// }
+// if(num1 === num2) { // == (( value )) === (( value | type ))
+//     console.log("They are equals");
+// } else {
+//     console.log("They are not equals");
+// }
+// var num1 = 10;
+// var num2 = 10;
+// if(num1 <= num2) {
+//     console.log("They are the same");
+// }
+// var myName = "Muhammad" + "Islam";
+// var studens = { // associative array
+//     'Muhammad' : 31,
+//     'Khaled' : 20,
+//     'Kamal' : 21,
+//     'Shahd' : 20
+// };
+// console.log(studens['Muhammad']);
+
+// var lastNumber = 10;
+
+
+// for(var i = 1;i <= lastNumber ; i++) {  // itteration
+//     console.log(i);
+// }
+
+// print with console.log() the numbers between 0 -> lastNumber
+// 1 2 3 4 5 6 7 8 9
+// 1 2 3 4 5 6 7 8 9 10
+
+// console.log(studens);
+
+// var students = [ // indexed array
+//     'Muhammad',
+//     'Khaled',
+//     'Kamal',
+//     'Shahd',
+//     'Omar',
+//     'Sara',
+//     'Samaa',
+// ];
+// for(var i = 1;i <= lastNumber ; i++) {  // itteration
+//     console.log(i);
+// }
+
+// for(var j = 0;j < students.length;j++) {
+//     console.log(students[j]);
+// }
+
+// let user1 = "Muhammad";
+// let user2 = "Omar saad";
+
+// if(user1 == "Muhammad" && user2 != "Omar") {
+//     console.log("Hello Admins");
+// } else {
+//     console.log('Hello');
+// }
+
+// Dont repeat your self; 
+
+// 10k users => Hello {his name}
+
+// var userOne     = "khaled";
+// var userTwo     = "Islam";
+// var userThree   = "Mohamed";
+
+
+
+// function calculate(num1, num2) {
+//     // console.log(num1+num2);
+//     var total = console.log(num1+num2);
+
+//     return total;
+// }
+
+
+// calculate(5,2);
